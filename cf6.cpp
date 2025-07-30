8af9c1a3d024e7d31f23a6ca619489cc37d0e140
talha-ibna-jaman <talhaibnajaman01@gmail.com>
2025-07-31 01:59:02 +0600

    Boy or Girl
    https://codeforces.com/problemset/problem/236/A

--- cf6.cpp
+++ cf6.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s;
-    getline(cin, s);
-    if(s.length() % 2 == 0){
-        cout << "CHAT WITH HER!" << endl;
+    string s; 
+    cin>>s;
+    unordered_set<char>st(s.begin(), s.end());
+    if(st.size()%2==0){
+        cout<<"CHAT WITH HER!"<<endl;
     } else {
-        cout << "IGNORE HIM!" << endl;
+        cout<<"IGNORE HIM!"<<endl;
     }
 }
