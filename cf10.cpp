90dc996bcc5a739867975eee8a70b422403f4b7c
talha-ibna-jaman <talhaibnajaman01@gmail.com>
2025-08-03 01:12:53 +0600

    Codeforces Checking
    https://codeforces.com/problemset/problem/1791/A

---
+++ cf10.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        char s;
+        cin >> s;
+    
+        if( s== 'c'||s=='o'||s=='d'||s=='e'||s=='f'||s=='r'||s=='s' )
+            cout << "YES" << endl;
+         else 
+            cout << "NO" << endl;
+    }
+        
+}
