ba43d2a3298febe76c3a6133230e70099c956c54
talha-ibna-jaman <talhaibnajaman01@gmail.com>
2025-08-06 02:13:43 +0600

    Odd One Out

---
+++ cf14.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a==b){
+            cout << c << endl;
+        } else if(b==c) {
+            cout << a << endl;
+        } else if(c==a) {
+            cout << b << endl;
+        } else {
+            cout << 0 << endl;
+        }
+    
+    }
+}
